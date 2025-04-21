ce96bb92b1029726d9382364d7f47b4fb04669f3
egg061714 <ttes0088@gmail.com>
2025-04-21 18:15:24 +0800

    更正路徑

--- code/main.py
+++ code/main.py
@@ -1,17 +1,26 @@
 import pygame
 import sys
-
+from pathlib import Path
 # 初始化 pygame
 pygame.init()
 
 # 設定視窗大小
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 760
+SCREEN_HEIGHT = 760
+playground = [SCREEN_WIDTH, SCREEN_HEIGHT]
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Pygame 基本範例")
 
-
-
+parent_path = Path(__file__).parents[1]
+print(parent_path)
+image_path = parent_path/ 'resource'
+icon_path = image_path/ 'icon.png'
+pygame.display.set_caption("Pygame 基本範例")
+icon =pygame.image.load(icon_path)
+pygame.display.set_icon(icon)
+background = pygame.Surface(screen.get_size())
+background=background.convert()
+background.fill((50,50,50))
 # 遊戲時鐘（用來控制FPS）
 fps=120
 clock = pygame.time.Clock()
@@ -26,6 +35,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    screen.blit(background,(0,0))
 
     pygame.display.update()  # 更新畫面
 
